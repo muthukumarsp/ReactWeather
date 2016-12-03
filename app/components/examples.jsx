d3120642f233b2d104c6da3784233cd5079bcefa
Muthu <muthukumar.perisamy@agriwebb.com>
2016-12-03 12:57:21 +1100

    Add foundation classes to main, nav pages

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,8 +1,18 @@
 var React = require('react');
+var {Link} = require('react-router');
+
 
 var Examples = (props) => {
     return (
-        <h3> Examples </h3>
+        <div>
+            <h1 classname="text-center"> Examples </h1>
+            <p> Here are few example locations to try out:</p>
+            <ol>
+                <li><Link to='/?location=Sydney'> Sydney,AU</Link></li>
+                <li> <Link to='/?location=Melbourne'> Melbourne,AU</Link></li>
+                <li> <Link to='/?location=Brisbane'> Brisbane,AU</Link></li>
+            </ol>
+        </div>
     );
 };
 module.exports = Examples;
