cf3e80a153d97bc9394a3295ac7042b53316e997
Muthu <muthukumar.perisamy@agriwebb.com>
2016-12-03 13:08:14 +1100

    Add content to "About" page

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -3,7 +3,20 @@ var React = require('react');
 //statless components
 var About = (props) => {
     return (
-        <h3> About Compoenent</h3>
+        <div>
+            <h1 className="text-center"> About Compoenent</h1>
+            <p> This is a weather application built on React. I have built async
+                this for the Complete React Web Developer Course.</p>
+
+            <p> Here are some of the tools I used:</p>
+
+            <ul>
+                <li><a href="https://facebook.github.io/react"> ReactJS - Java script framework used. </a></li>
+                <li><a href="http://openweathermap.org"> Open Weather Map to search for weather data by citi name </a></li>
+                <li><a href="http://foundation.zurb.com"> Foundation - CSS framework </a>
+                </li>
+            </ul>
+        </div>
     );
 };
 
